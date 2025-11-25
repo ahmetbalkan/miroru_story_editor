59e6c12515c2b156ca3b8b3a74cd0a15b26ec341
Ahmet R. BALKAN <48199986+ahmetbalkan@users.noreply.github.com>
2025-11-25 16:00:52 +0300

    son versiona guncellendi.

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -67,7 +67,8 @@ import 'l10n_zh.dart';
 /// be consistent with the languages listed in the L10n.supportedLocales
 /// property.
 abstract class L10n {
-  L10n(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+  L10n(String locale)
+    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -87,12 +88,13 @@ abstract class L10n {
   /// Additional delegates can be added by appending to this list in
   /// MaterialApp. This list does not have to be used at all if a custom list
   /// of delegates is preferred or required.
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
-    delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ];
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
@@ -102,7 +104,7 @@ abstract class L10n {
     Locale('ko'),
     Locale('ru'),
     Locale('tr'),
-    Locale('zh')
+    Locale('zh'),
   ];
 
   /// Label for a button to start drawing on the canvas.
@@ -151,30 +153,43 @@ class _L10nDelegate extends LocalizationsDelegate<L10n> {
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['en', 'hi', 'ja', 'ko', 'ru', 'tr', 'zh'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => <String>[
+    'en',
+    'hi',
+    'ja',
+    'ko',
+    'ru',
+    'tr',
+    'zh',
+  ].contains(locale.languageCode);
 
   @override
   bool shouldReload(_L10nDelegate old) => false;
 }
 
 L10n lookupL10n(Locale locale) {
-
-
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
-    case 'en': return L10nEn();
-    case 'hi': return L10nHi();
-    case 'ja': return L10nJa();
-    case 'ko': return L10nKo();
-    case 'ru': return L10nRu();
-    case 'tr': return L10nTr();
-    case 'zh': return L10nZh();
+    case 'en':
+      return L10nEn();
+    case 'hi':
+      return L10nHi();
+    case 'ja':
+      return L10nJa();
+    case 'ko':
+      return L10nKo();
+    case 'ru':
+      return L10nRu();
+    case 'tr':
+      return L10nTr();
+    case 'zh':
+      return L10nZh();
   }
 
   throw FlutterError(
     'L10n.delegate failed to load unsupported locale "$locale". This is likely '
     'an issue with the localizations generation tool. Please file an issue '
     'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.'
+    'that was used.',
   );
 }
